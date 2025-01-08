4bed789bd7d76c65838c6565775bd2fd9d1e2cc3
atrajit-sarkar <atrajit.sarkar@gmail.com>
2025-01-08 11:14:55 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const blogs = [
+    { title: 'Infinite Dimension Vector Spaces Anomaly', url: 'Blogs\\InfiniteDimensionVectorSpacesAnomaly\\content.html' },
+
+
+];
+
+const blogList = document.getElementById('blog-list');
+
+blogs.forEach(blog => {
+    const listItem = document.createElement('li');
+    const link = document.createElement('a');
+    link.textContent = blog.title;
+    link.href = blog.url;
+    listItem.appendChild(link);
+    blogList.appendChild(listItem);
+});
