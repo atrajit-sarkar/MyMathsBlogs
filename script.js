9b06623a04099a5abc82471a5e41af3d502d0029
atrajit-sarkar <155407021+atrajit-sarkar@users.noreply.github.com>
2025-03-05 19:11:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const blogs = [
     { title: 'Infinite Dimension Vector Spaces Anomaly', url: 'Blogs\\InfiniteDimensionVectorSpacesAnomaly\\index.html' },
     { title: 'CoBoAoPhi Preserves Imaginary Axis Pointwise', url: 'Blogs\\CoBoAoPhi_Preservers_Imaginary\\index.html' },
+    { title: 'My Works', url: 'https://mymathworld.gongobongo.org/' },
 
 ];
 
